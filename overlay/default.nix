c0ae5b6bf5eaa75561f1cede3bfe6b0e25a9ddd9
Michał Sidor <public+git@meekchopp.es>
2021-02-14 00:12:52 +0100

    Move overlay packages out of separate directories

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,11 +1,11 @@
 self: super:
 {
-  ipmi-exporter = super.callPackage ./ipmi-exporter { };
-  meekchoppes = super.callPackage ./meekchoppes { };
-  rustagit = super.callPackage ./rustagit { };
-  umiarkonowy = super.callPackage ./umiarkonowy { };
-  scoobideria = super.callPackage ./scoobideria { };
-  sccache-dist = super.callPackage ./sccache-dist { };
+  ipmi-exporter = super.callPackage ./ipmi-exporter.nix { };
+  meekchoppes = super.callPackage ./meekchoppes.nix { };
+  rustagit = super.callPackage ./rustagit.nix { };
+  umiarkonowy = super.callPackage ./umiarkonowy.nix { };
+  scoobideria = super.callPackage ./scoobideria.nix { };
+  sccache-dist = super.callPackage ./sccache-dist.nix { };
   honk = super.callPackage ./honk.nix { };
   czy-piec-siedem = super.callPackage ./czy-piec-siedem.nix { };
 }
