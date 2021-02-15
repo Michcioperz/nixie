a7f2052c652c81c7214eaba08571bd445606c8a3
Michał Sidor <michcioperz@hinata.iscute.ovh>
2021-02-15 22:37:53 +0100

    add nameservers to grafana container

--- hinata.nix
+++ hinata.nix
@@ -350,6 +350,7 @@ let secrets = (import /etc/nixos/secrets.nix); in
       networking.firewall.allowedTCPPorts = [ 3000 ];
       networking.defaultGateway = "192.168.7.1";
       networking.interfaces.eth0.ipv4.addresses = [ { address = "192.168.7.4"; prefixLength = 24; } ];
+      networking.nameservers = [ "1.1.1.1" ];
     };
   };
 
